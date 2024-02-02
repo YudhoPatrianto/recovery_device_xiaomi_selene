c01d9d47bd4ecf0341fd260289483557e62c56f5
YudhoPatrianto <kydh01123@gmail.com>
2024-02-03 01:38:32 +0700

    selene: BoardConfig:absolote
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- device.mk
+++ device.mk
@@ -26,14 +26,9 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
 
-PRODUCT_PACKAGES += \
-    bootctrl.mt6768
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6768 \
-    libgptutils \
-    libz \
-    libcutils
+PRODUCT_PACKAGES := \
+   bootctrl.mt6768 \
+   bootctrl.mt6768.recovery
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
